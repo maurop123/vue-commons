c2dc39b1e1ba102d9595948b6e1ab8ffcba66666
mauro <maurop123@gmail.com>
2018-03-21 15:04:06 -0300

    exporting a bunch more mutation functions in store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,42 @@
 import { db } from '@/database'
 
 export default {
+  pop,
+  push,
+  update,
+  updateWithId,
+  updateCollection,
   getCollection,
+}
+
+export function pop(key) {
+  return (state, payload) => state[key].pop(payload)
+}
+
+export function push(key) {
+  return (state, payload) => state[key].push(payload)
+}
+
+export function update(key) {
+  return (state, payload) => state[key] = payload
+}
+
+export function set(key) {
+  return (state, payload) => state[key] = payload
+}
+
+export function updateWithId(key) {
+  return (state, payload) => state[key] = state[key].map(m => payload.id === m.id ? payload : m)
+}
+
+export function updateCollection(key) {
+  return (state, payload) => {
+    if (state.find(x => x.id === payload.id)) {
+      return updateWithId(key)(state, payload)
+    } else {
+      return push(key)(state, payload)
+    }
+  }
 }
 
 export function getCollection(opts = {
@@ -10,7 +45,8 @@ export function getCollection(opts = {
   mutation: null,
 }, done) {
   const { key, path, mutation } = opts
-  return ({state, commit}) => {
+  return (context) => {
+    const {state, commit} = context
     if (key === null && path === null) {
       throw new Error('Need collection key or path. Both are null.')
     } else {
@@ -20,7 +56,7 @@ export function getCollection(opts = {
           if (mutation) {
             commit(mutation, col)
           } else {
-            done(col)
+            done(context, col)
           }
         } else {
           console.log(`Nothing returned at ${path}`)
