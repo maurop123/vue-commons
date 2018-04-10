c3e544b5d3439765482713e1c40763ceb99f4487
mauro <maurop123@gmail.com>
2018-04-10 15:28:08 -0400

    add flex-card, grid-list, and horizontal-scroller

---
+++ src/components/FlexCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-flex>
+    <v-card class="card">
+      <v-card-media
+        v-if="img && img.src"
+        :src="img.src"
+        height="300px"
+        :class="img.classes"
+      />
+      <v-card-title v-if="title">
+        <h3 class="headline">{{ title }}</h3>
+      </v-card-title>
+      <v-card-text v-if="description"
+        class="truncate"
+      >
+        <div>
+          {{ description }}
+          <!-- <span class="ellipses"></span> -->
+        </div>
+        <div class="fade"></div>
+      </v-card-text>
+      <v-card-actions v-if="link && link.url">
+        <v-btn v-if="link.source === 'medium'"
+          flat block
+          class="green--text"
+          :href="link.url"
+        >
+          Read More on Medium
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+  export default {
+    name: 'flex-card',
+    props: {
+      title: String,
+      description: String,
+      img: Object,
+      link: Object,
+    },
+  }
+</script>
+
+<style scoped>
+  .card {
+    min-width: 350px;
+  }
+
+  .animate {
+    transition: all .25s ease;
+  }
+
+  .contain >>> .card__media__background {
+    background-size: contain !important;
+  }
+
+  .truncate {
+    /* max-height: 150px; */
+    max-height: 9em;
+    overflow: hidden;
+    /* doesn't work
+    text-overflow: ellipsis; */
+  }
+
+  .fade {
+    position: absolute;
+    bottom: 52px;
+    left: 0;
+    height: 50px;
+    width: 100%;
+    background: linear-gradient(rgba(255,255,255,0), rgba(255,255,255,1));
+  }
+</style>
+
+https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FbEjLIhTDqJRaE%2Fgiphy.gif
--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,19 @@
-import MasonryGrid from './MasonryGrid'
+import FlexCard from './PostCard'
 import PostCard from './PostCard'
+import GridList from './layouts/GridList'
+import MasonryGrid from './layouts/MasonryGrid'
+import HorizontalScroller from './layouts/HorizontalScroller'
 import VertSplitPosts from './layouts/VertSplitPosts'
 import SimplePosts from './layouts/SimplePosts'
 
 export default {
-  MasonryGrid,
+  FlexCard,
   PostCard,
   layouts: {
+    GridList,
+    MasonryGrid,
     SimplePosts,
     VertSplitPosts,
+    HorizontalScroller,
   },
 }
